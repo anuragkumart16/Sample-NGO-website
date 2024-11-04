03263af68144f830c25ed6099c3a16da59b4a8f0
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2024-11-04 09:05:04 +0530

    added changes

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 // manipulating the navbar
+let menuBtn = document.getElementById('menu-btn')
+let menu = document.getElementById('menu')
 let header = document.getElementById('header')
-if (window.innerWidth == 600){
-    header.style.flexDirection = 'Column'
+let closeBtn = document.getElementById('close-btn')
+
+if(window.innerWidth<=600){
+    menuBtn.style.display = 'block';
+}else{
+    menuBtn.style.display = 'none';
+}
+
+
+
+function openMenu(){
+    menu.style.display ='flex';
+    header.style.flexDirection ='column';
+    menuBtn.style.display='none';
+    closeBtn.style.display ='block';
+}
+function closeMenu(){
+    menu.style.display ='none';
+    header.style.flexDirection ='row';
+    menuBtn.style.display = 'block';
+    closeBtn.style.display = 'none';
+
 }
